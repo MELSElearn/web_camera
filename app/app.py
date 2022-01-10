1a0d376180d03b7885b7b19c9e7713564fff3558
MELSElearn <78197595+MELSElearn@users.noreply.github.com>
2022-01-10 12:26:43 +0800

    Update app.py

--- app/app.py
+++ app/app.py
@@ -11,4 +11,5 @@ def hello_world():
         return render_template('index.html', user_image = '')
     else:
         txt64 = request.form['txt64']
-        return render_template('index.html', user_image = '')
+        encoded_data = txt64.split(',')[1]
+        return render_template('index.html', user_image = encoded_data)
