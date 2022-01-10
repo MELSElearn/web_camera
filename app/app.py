a278b48613ee0782e7be82627d33367bd3d2217c
MELSElearn <78197595+MELSElearn@users.noreply.github.com>
2022-01-10 18:28:14 +0800

    Update app.py

--- app/app.py
+++ app/app.py
@@ -35,5 +35,19 @@ def hello_world():
     
 @app.route("/api/info")
 def api_info():
-    im_b64 = 'asdasdads'
+    txt64 = request.args.get("todo")
+    encoded_data = txt64.split(',')[1]
+    encoded_data = b64decode(encoded_data)
+    nparr = np.frombuffer(encoded_data, np.uint8)
+    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    detector = HandDetector(detectionCon=0.8, maxHands=2)
+    hands, img = detector.findHands(img)
+    totalfingers=0
+    if hands:
+        fingers = detector.fingersUp(hands[0])
+        totalfingers = fingers.count(1)
+    cv2.putText(img,f'{int(totalfingers)}',(50,70), cv2.FONT_HERSHEY_PLAIN,5,(250,0,0),5)
+    _, im_arr = cv2.imencode('.png', img)
+    im_bytes = im_arr.tobytes()
+    im_b64 = b64encode(im_bytes).decode("utf-8")
     return im_b64
